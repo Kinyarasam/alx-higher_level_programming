9c30c0c3e4803ef165333034dca2f469d2b6cda5
kinyarasam <samkinyara2000@gmail.com>
2022-06-02 21:23:07 +0300

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    """Implements the bytecode.
+
+    Args:
+        a: integer
+        b: integer
+
+    Returns:
+        The return value id:
+            add(c, i) if TRUE
+            sub(a, b) if FALSE
+
+    """
+    from magic_calculation_102 import add, sub
+    if (a < b):
+        c = add(c, i)
+        return (c)
+    else:
+        return(sub(a, b))
