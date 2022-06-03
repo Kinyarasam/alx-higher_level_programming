3aa0b286cbf4df74caef10b53dc9f787ca78be98
kinyarasam <samkinyara2000@gmail.com>
2022-06-03 16:45:12 +0300

    6-print_matrix_integer.py

--- 0x03-print_list_integer.py/6-print_matrix_integer.py
+++ 0x03-print_list_integer.py/6-print_matrix_integer.py
@@ -2,10 +2,11 @@
 def print_matrix_integer(matrix=[[]]):
     """prints a matrix of integers
     """
-    #for row in matrix:
-    #    print("{:d} {:d} {:d}".format(row[0], row[1], row[2]))
-    #else:
-    #    print("")
+    """for row in matrix:
+        print("{:d} {:d} {:d}".format(row[0], row[1], row[2]))
+    else:
+        print("")
+    """
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             print("{:d}".format(matrix[row][col]), end="")
