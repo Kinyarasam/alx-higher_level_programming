90989eea1b27384a33d50afc82cf600463232bf0
kinyarasam <samkinyara2000@gmail.com>
2022-06-04 07:43:10 +0300

    pycodestyle-check

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,7 +2,7 @@
 def print_reversed_list_integer(my_list=[]):
     """prints all integers of a list in reverse order
     """
-    if my_list == None:
+    if my_list is None:
         return None
     else:
         for num in my_list[::-1]:
