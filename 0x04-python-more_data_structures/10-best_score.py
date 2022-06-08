44ce8344264a8c13cc0f02534b839a7acf8f09dd
kinyarasam <samkinyara2000@gmail.com>
2022-06-08 21:47:23 +0300

    10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+def best_score(a_dictionary):
+    """Returns a key with the biggest integer value
+    Args:
+        a_dictionary: dictionary
+
+    Returns:
+        if no score is round, the return value is none
+        otherwise, the biggest value
+    """
+    if a_dictionary:
+        return max(a_dictionary, key = a_dictionary.get)
