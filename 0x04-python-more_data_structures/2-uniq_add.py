6d144272935622d74bb97f435d44ce7950e17911
kinyarasam <samkinyara2000@gmail.com>
2022-06-08 13:59:16 +0300

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     """Adds all uniq integers in a list
-    
+
     Args:
 
     Returns:
@@ -9,5 +9,5 @@ def uniq_add(my_list=[]):
     """
     sum = 0
     for num in set(my_list):
-        sum += num    
+        sum += num
     return sum
