a6cb925ecbbcac28f8be3cd0ed4a576f9c13e738
kinyarasam <samkinyara2000@gmail.com>
2022-06-21 22:07:12 +0300

    2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""class Square - A class that defines a square
+"""
+
+
+class Square:
+    """class representing a square object
+    """
+    def __init__(self, size=0):
+        """class constructor
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Calculate the area of the square
+        """
+        sq_area = self.__size ** 2
+        return (sq_area)
