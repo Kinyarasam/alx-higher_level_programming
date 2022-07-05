913a9b2845317bd4ad5a62542852c9f02cde7784
kinyarasam <samkinyara2000@gmail.com>
2022-07-05 15:05:16 +0300

    Pycodestyle code fix

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """ Module 1-write_file
 Contains function that creates file and returns num chars written
 """
+
+
 def write_file(filename="", text=""):
     """ writes to text file and returns the number of characters written """
     with open(filename, mode="w", encoding="utf-8") as f:
