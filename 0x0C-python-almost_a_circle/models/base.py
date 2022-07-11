269b8f0880d19ac92bcee0e50f80b919a0534ace
kinyarasam <samkinyara2000@gmail.com>
2022-07-11 19:23:03 +0300

    Returns the JSON string representation. JSON is one of the standards of sharing data represeentation. Update the class Base. Adding the static method def to_json_string(list_dictionaries) 14-main.py - tests files Update the Base tests

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Defines a base model class """
+import json
 
 
 class Base:
@@ -22,3 +23,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """ Returns the JSON string representation.
+
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries )
