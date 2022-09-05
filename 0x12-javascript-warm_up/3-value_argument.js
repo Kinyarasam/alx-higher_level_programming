cf0c5161c32dd9f4c8e4f004f59fceef7e5e6309
kinyarasam <samkinyara2000@gmail.com>
2022-09-05 14:50:42 +0300

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 // Script that prints the first argument passed to it:
+let output;
 
-(process.argv[2] ? console.log(process.argv[2]) : console.log('No arguement'));
+(process.argv[2] ? output = process.argv[2] : output = 'No arguement');
+console.log(output);
