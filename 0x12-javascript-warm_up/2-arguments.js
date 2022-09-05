0566f22cfdcec4c1034293653f049304735aba07
kinyarasam <samkinyara2000@gmail.com>
2022-09-05 14:47:31 +0300

    prints a message depending of the number of arguments passed:

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 // Script that prints a message depending of the number of arguments passed:
-let result; 
+let result;
 (process.argv.length < 3 ? result = 'No arguement' : process.argv.length === 3 ? result = 'Argument found' : result = 'Arguements found');
 console.log(result);
