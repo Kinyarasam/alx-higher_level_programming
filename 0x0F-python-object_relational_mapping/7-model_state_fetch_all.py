e7b2fdb9471d5d9e0cb9c87c31833abf4de6eade
kinyarasam <samkinyara2000@gmail.com>
2022-09-13 14:26:58 +0300

    Script that aadds the State object Louisiana to the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,10 +12,14 @@ from model_state import State
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
+    if len(sys.argv) != 4:
+        print('Usage: ./7-model_state_fetch_all.py <mysql username> <mysql \
+password> <database name>')
+    else:
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
 '.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    for state in session.query(State).order_by(State.id):
-        print("{}: {}".format(state.id, state.name))
+        for state in session.query(State).order_by(State.id):
+            print("{}: {}".format(state.id, state.name))
