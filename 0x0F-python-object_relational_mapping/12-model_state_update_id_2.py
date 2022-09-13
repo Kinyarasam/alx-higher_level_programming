de7bc6212a3a2602fb45d3370a778efcfbb84f1f
kinyarasam <samkinyara2000@gmail.com>
2022-09-13 14:36:22 +0300

    script that changes the name of a State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,8 +3,8 @@
 Script that adds the State object "Louisiana" to the database hbtn_0e_6_usa
 
 Usage:
-    ./11-model_state_insert.py <mysql username> <mysql password> <database \
-name>
+    ./12-model_state_update_id_2.py <mysql username> <mysql password> \
+<database name>
 """
 import sys
 from sqlalchemy import create_engine
@@ -14,15 +14,14 @@ from model_state import State
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
-        print('./11-model_state_insert.py <mysql username> <mysql password \
-> <database name>')
+        print('./12-model_state_upadate_id_2.py <mysql username> <mysql \
+password> <database name>')
     else:
         engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}\
 '.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        add_State = State(name='Louisiana')
-        session.add(add_State)
+        update_State = session.query(State).filter_by(id=2).first()
+        update_State.name = "New Mexico"
         session.commit()
-        print(add_State.id)
