f32a041ebd64523fc0a7c8d260b1d3f19461ca6d
kinyarasam <skinyara.30@gmail.com>
2022-09-26 17:22:22 +0300

    sends a POST request to the passed URL with the email as a parameter, and finally displays the body of the response.

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+
+Usage:
+    ./6-post_email.py <url> <email>
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: ./6-post_email.py <url> <email>")
+    else:
+        URL = sys.argv[1]
+        
+        value = {
+                    "email": sys.argv[2]
+                }
+        r = requests.post(URL, data=value)
+        print(r.text)
