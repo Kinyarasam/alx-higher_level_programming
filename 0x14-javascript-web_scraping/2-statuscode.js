0590463c1386b5c3b108a5e5c25181dbffb5300a
kinyarasam <skinyara.30@gmail.com>
2022-10-19 21:07:07 +0300

    Display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,6 +4,7 @@
 const request = require('request');
 const URL_LINK = process.argv[2];
 
-request(URL_LINK, (response, data) => {
+request(URL_LINK, (error, response, data) => {
   console.log('code:', response.statusCode); // Print the response status code if a response was received
+  if (error) throw error;
 });
