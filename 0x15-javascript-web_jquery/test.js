53a59726a5d420a958bc320a76f86ff86cf60548
kinyarasam <skinyara.30@gmail.com>
2022-10-27 17:14:14 +0300

    Starwars movies

--- 0x15-javascript-web_jquery/test.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/node
-
-const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json'
-request(url, (data, response, error) =>{
-	if (error) throw error;
-	console.log(data);
-});
